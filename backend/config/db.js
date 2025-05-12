2adfc6cb39b1a1a1f8541f70adb852225a71bfaa
shahriaranto1085 <126331795+shahriaranto1085@users.noreply.github.com>
2025-05-12 16:01:27 +0600

    Create db.js

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+require('dotenv').config();  // This loads the environment variables from the .env file
+
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
