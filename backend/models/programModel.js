0cd5d98a03ea8639094b682af1162ee1edb874f6
ahmedmuntasir <ahmed.muntasir@g.bracu.ac.bd>
2025-05-22 01:06:41 +0600

    programme model

---
+++ backend/models/programModel.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const programSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    duration: {
+      type: Number, // Duration in weeks
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ['trainer', 'nutritionist'], // Role of the creator
+      required: true,
+    },
+    creator: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User', // Reference to the user who created the program
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Program', programSchema);
